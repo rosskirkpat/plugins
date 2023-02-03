dc070050d678297a636f41419426aae4e46ad1dd
Ross Kirkpatrick <rosskirkpat@outlook.com>
2023-02-03 14:10:33 -0500

    windows builds

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
+	win_bridge "github.com/containernetworking/plugins/plugins/main/windows/win-bridge"
+	win_overlay "github.com/containernetworking/plugins/plugins/main/windows/win-overlay"
 	"os"
 	"path/filepath"
 
 	"github.com/containernetworking/plugins/plugins/ipam/host-local"
-	"github.com/containernetworking/plugins/plugins/main/bridge"
-	"github.com/containernetworking/plugins/plugins/main/loopback"
 	"github.com/containernetworking/plugins/plugins/meta/flannel"
-	"github.com/containernetworking/plugins/plugins/meta/portmap"
 	"github.com/docker/docker/pkg/reexec"
 )
 
 func main() {
 	os.Args[0] = filepath.Base(os.Args[0])
 	reexec.Register("host-local", hostlocal.Main)
-	reexec.Register("bridge", bridge.Main)
+	reexec.Register("bridge", win_bridge.Main)
+	reexec.Register("overlay", win_overlay.Main)
 	reexec.Register("flannel", flannel.Main)
-	reexec.Register("loopback", loopback.Main)
-	reexec.Register("portmap", portmap.Main)
 	reexec.Init()
 }
--- plugins/main/windows/win-overlay/win-overlay_windows.go
+++ plugins/main/windows/win-overlay/win-overlay_windows.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package win_overlay
 
 import (
 	"encoding/json"
@@ -172,6 +172,6 @@ func cmdCheck(_ *skel.CmdArgs) error {
 	return nil
 }
 
-func main() {
+func Main() {
 	skel.PluginMain(cmdAdd, cmdCheck, cmdDel, version.All, bv.BuildString("win-overlay"))
 }
